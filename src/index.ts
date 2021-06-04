83843da9288cc724ef07bfbd607c083849c4a17e
hirokita117 <hiroaki.kitai@stafes.com>
2021-06-04 17:11:54 +0900

    inferを記述してみた

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 const first: string = 'aaaa';
 console.log(first);
+
+type AnalogClock = {
+  longHand: string;
+}
+
+type DigitalClock = {
+  longHand: number;
+}
+
+type LongHand<T> = T extends { longHand: infer U } ? U : never;
+
+const longHandString: LongHand<AnalogClock> = '12:00';
+const longHandNumber: LongHand<DigitalClock> = 12;
+
+console.log([
+  longHandString, longHandNumber
+]);
