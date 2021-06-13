31604789e3da316f16916111e92ae4153511168a
hirokita117 <r4.henke@gmail.com>
2021-06-13 22:05:11 +0900

    色味をつけてみた

--- src/GoF/State/main.ts
+++ src/GoF/State/main.ts
@@ -1,3 +1,4 @@
+import * as colors from "colors";
 import * as readlineSync from "readline-sync";
 
 import { SafeFrame } from "./SafeFrame";
@@ -8,9 +9,9 @@ for (let hour = 0; hour < 24; hour++) {
   try {
     frame.setClock(hour);
     if (readlineSync.keyInYN("テスト")) {
-      console.log("yes");
+      console.log(colors.bgGreen("yes"));
     } else {
-      console.log("no");
+      console.log(colors.bgRed("no"));
     }
   } catch (error) {
     console.log(error);
